94e72840b2e7c308f3588a61c1f668b96db7d61d
olathedev <olapademicheal3@gmail.com>
2024-08-28 03:06:16 -0700

    Ui fix in auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,8 +8,8 @@ type Props = {
 const layout = ({ children }: Props) => {
     return (
         <div className='w-full flex h-screen'>
-            <div className="w-full overflow-auto lg:w-1/2 flex justify-center items-center pt-10">
-                <div className='w-[440px] flex flex-col gap-8'>
+            <div className="w-full overflow-y-auto lg:w-1/2 flex justify-center items-center pt-10">
+                <div className='w-full px-4 md:w-[440px] flex flex-col gap-8'>
                     <Image src='/logo.png' alt='' height={40} width={90} />
                     {children}
                 </div>
