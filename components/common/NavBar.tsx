dd77e97a6c127ae3352da6ae36318a775d697d77
olathedev <olapademicheal3@gmail.com>
2024-09-03 03:54:03 -0700

    mobile nav implementation

--- components/common/NavBar.tsx
+++ components/common/NavBar.tsx
@@ -1,15 +1,20 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import HamburgerIcon from './icons/HamburgerIcon'
 
 type Props = {}
 
 const NavBar = (props: Props) => {
+    const [isOpen, setIsOpen] = useState(false)
     return (
-        <nav className="h-[75px] w-full sticky top-0 bg-appBackground z-20">
+        <nav className="h-[75px] w-full sticky top-0 bg-appBackground z-20 ">
             <div className='hidden md:flex container mx-auto h-full justify-between items-center px-10'>
 
+                <div className="">
+                    <Image src='/logo.png' alt='' height={40} width={90} />
+                </div>
 
                 <ul className="flex space-x-8 text-white">
                     <li className='text-primary-100'>Home</li>
@@ -27,17 +32,45 @@ const NavBar = (props: Props) => {
 
             {/* Mobile Nav */}
 
-            <div className='container mx-auto flex justify-between p-4'>
+            <div className='md:hidden container mx-auto flex flex-col bg-appBackground shadow-lg'>
+                <div className='flex justify-between items-center py-4 px-4'>
 
-                <div>
-                    <HamburgerIcon />
+                    <div className='cursor-pointer' onClick={() => setIsOpen(!isOpen)}>
+                        {isOpen && (
+                            <span className="text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                </svg>
+
+                            </span>
+                        )}
+                        {!isOpen && <HamburgerIcon />}
+
+
+                    </div>
+
+                    <div className="">
+                        <Image src='/logo.png' alt='' height={40} width={90} />
+                    </div>
+
                 </div>
 
-                <div className="">
-                    <Image src='/logo.png' alt='' height={40} width={90} />
-                </div>
+                {isOpen && (
+                    <div className="p-4 flex flex-col gap-3">
+                        <ul className="flex flex-col space-y-2 text-white">
+                            <li className='text-primary-100'>Home</li>
+                            <li className='te'>How it Works </li>
+                            <li className='text'>About Us</li>
+                            <li className='text'>Contact </li>
+                        </ul>
+                        <Link href='/signup'><button className='w-full bg-primary-100 text-white py-3 px-10 rounded-lg'>Sign up</button></Link>
+
+
+                    </div>
+                )}
 
             </div>
+
         </nav>
     )
 }
--- components/landingPage/Hero.tsx
+++ components/landingPage/Hero.tsx
@@ -8,8 +8,8 @@ type Props = {}
 const montserat = Montserrat({ subsets: ['latin'] })
 const Hero = (props: Props) => {
     return (
-        <div className='relative container mx-auto px-6 my-6 md:px-20 md:my-20 md:flex gap-10'>
-            <div className="md:w-[55%] flex flex-col gap-2 text-white">
+        <div className='relative container mx-auto px-6 my-6 md:px-20 md:mt-20 md:flex gap-10'>
+            <div className="md:w-[60%] flex flex-col gap-2 text-white">
                 <h1 className={`${montserat.className} text-3xl md:text-[53px] font-bold md:leading-[65px] leading-10`}>
                     Welcome to Flipr, your gateway to <span className='text-primary-200'>hassle-free crypto </span> arbitrage with up to 10% weekly Returns
                 </h1>
@@ -22,8 +22,8 @@ const Hero = (props: Props) => {
                     </div>
 
                     <div className="pt-5 w-full flex justify-center gap-4">
-                        <button className='bg-primary-100 text-white py-3 px-8 md:px-10 rounded-lg'>Join Flipr</button>
-                        <button className='border border-primary-100 text-white py-3 px-8 md:px-10 rounded-lg'>Trade Crypto</button>
+                        <button className='bg-primary-100 text-white py-3 md:py-4 px-8 md:px-10 rounded-lg'>Join Flipr</button>
+                        <button className='border border-primary-100 text-white py-3 md:py-4 px-8 md:px-10 rounded-lg'>Trade Crypto</button>
 
                     </div>
                 </div>
